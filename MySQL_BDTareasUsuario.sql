e9d3dd4d51c0b8addaa818e94b0701c0c0da4b8d
Hugo Vargas <hugo.vargas1772@gmail.com>
2024-03-01 17:22:39 -0500

    Creando V2 de 'bdtareasusuario'

--- MySQL_BDTareasUsuario.sql
+++ MySQL_BDTareasUsuario.sql
@@ -1,3 +1,32 @@
 CREATE DATABASE IF NOT EXISTS bdtareasusuario;
 
 USE bdtareasusuario;
+
+CREATE TABLE usuario (
+    idusuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    correo VARCHAR(50),
+    clave VARCHAR(16),
+    tipo VARCHAR(25) CHECK (tipo IN ('administrador','cliente'))
+);
+
+CREATE TABLE tarea (
+    idtarea INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(50)
+);
+CREATE TABLE encuesta (
+    idencuesta INT AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(1000),
+    idtarea INT,
+    FOREIGN KEY (idtarea) REFERENCES tarea(idtarea)
+);
+
+CREATE TABLE usuarioencuesta (
+    idusuario INT,
+    idencuesta INT,
+    PRIMARY KEY (idusuario,idencuesta),
+    FOREIGN KEY (idusuario) REFERENCES usuario(idusuario),
+    FOREIGN KEY (idencuesta) REFERENCES encuesta(idencuesta),
+    estado VARCHAR(10) CHECK (estado IN ('pendiente','completado')),
+    disponibilidad CHAR(1) CHECK (disponibilidad IN ('0','1'))
+);
