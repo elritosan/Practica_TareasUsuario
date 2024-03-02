b5929672d7bcfbcda75183a088ef38136faaee38
Hugo Vargas <hugo.vargas1772@gmail.com>
2024-03-01 19:23:01 -0500

    Modificando 'bdtareasusuario' (DELETE y UPDATE en Cascada)

--- MySQL_BDTareasUsuario.sql
+++ MySQL_BDTareasUsuario.sql
@@ -18,15 +18,15 @@ CREATE TABLE encuesta (
     idencuesta INT AUTO_INCREMENT PRIMARY KEY,
     descripcion VARCHAR(1000),
     idtarea INT,
-    FOREIGN KEY (idtarea) REFERENCES tarea(idtarea)
+    FOREIGN KEY (idtarea) REFERENCES tarea(idtarea)  ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE usuarioencuesta (
     idusuario INT,
     idencuesta INT,
     PRIMARY KEY (idusuario,idencuesta),
-    FOREIGN KEY (idusuario) REFERENCES usuario(idusuario),
-    FOREIGN KEY (idencuesta) REFERENCES encuesta(idencuesta),
+    FOREIGN KEY (idusuario) REFERENCES usuario(idusuario) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idencuesta) REFERENCES encuesta(idencuesta) ON DELETE CASCADE ON UPDATE CASCADE,
     estado VARCHAR(10) CHECK (estado IN ('pendiente','completado')),
     disponibilidad CHAR(1) CHECK (disponibilidad IN ('0','1'))
 );
